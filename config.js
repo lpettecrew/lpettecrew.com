52661d4471a590112fef0671154a353c829d637f
lpettecrew <leo@nexus-sites.com>
2021-04-09 00:33:48 +0100

    Fixed stuff

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://lpettecrew-com.vercel.app";
+// export const server = dev
+//   ? "http://localhost:3000"
+//   : "https://lpettecrew-com.vercel.app";
+
+export const server = "http://localhost:3000";
